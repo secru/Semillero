039ffe5e282701fda35b5144ef1ff9a85e8d8180
Sebastian Cruz Duarte <scruz@asesoftware.com>
2019-08-30 15:38:12 -0500

    arreglo jquery

--- js/app.js
+++ js/app.js
@@ -1,19 +1,21 @@
-clicked = true
-$("#boton").click(function () {
-    if (clicked) {
-        $(this).css('background-color', 'gray');
-        clicked = false
-    } else {
-        $(this).css('background-color', '#8fc02c');
-        clicked = true
-    }
-});
-
-$(".item").hover(
-    function () {
-        var iid = $(this).attr("id");
-        $("#" + iid).find(".saber-mas").css('background-color', '#a6da3f')
-    }, function(){
-        var iid = $(this).attr("id");
-        $("#" + iid).find(".saber-mas").css('background-color', '')
+$(function () {
+    clicked = true
+    $("#boton").click(function () {
+        if (clicked) {
+            $(this).css('background-color', 'gray');
+            clicked = false
+        } else {
+            $(this).css('background-color', '#8fc02c');
+            clicked = true
+        }
     });
+
+    $(".item").hover(
+        function () {
+            var iid = $(this).attr("id");
+            $("#" + iid).find(".saber-mas").css('background-color', '#a6da3f')
+        }, function () {
+            var iid = $(this).attr("id");
+            $("#" + iid).find(".saber-mas").css('background-color', '')
+        });
+});
